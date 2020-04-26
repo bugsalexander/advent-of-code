6fdc3262599af3afe90c4a9ac634244c1ee1766b
meebs <alex@takayamasan.com>
2020-04-26 00:55:02 -0700

    solve the problem + fmt

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,25 +1,26 @@
 use std::iter::FromIterator;
 use std::str::FromStr;
 
+// include the tests module for testing
 mod tests;
 
-// represents a type of direction
+/// represents a type of direction
 #[derive(PartialEq, Debug)]
-pub enum DirKind {
+enum DirKind {
     Left,
     Right,
     Up,
     Down,
 }
 
-// represents a direction, with a magnitude
+/// represents a direction, with a magnitude
 #[derive(PartialEq, Debug)]
 struct Dir {
     dist: i32,
     kind: DirKind,
 }
 
-// parses the input into two separate lists of directions
+/// parses the input into two separate lists of directions
 fn parse(input: &str) -> (Vec<Dir>, Vec<Dir>) {
     let mut parts = input.trim().split("\n");
     let dirs_1 = parse_dirs(parts.next().unwrap());
@@ -27,13 +28,13 @@ fn parse(input: &str) -> (Vec<Dir>, Vec<Dir>) {
     (dirs_1, dirs_2)
 }
 
-// parse the dirs into an Iterator of dirs
+/// parse the dirs into an Iterator of dirs
 fn parse_dirs(input: &str) -> Vec<Dir> {
     // if any of them fail, panic
     Vec::from_iter(input.split(",").map(|s| s.parse::<Dir>().unwrap()))
 }
 
-// parse strings of the form (R|L|U|D)\d+ into Dirs
+/// parse strings of the form (R|L|U|D)\d+ into Dirs
 impl FromStr for Dir {
     type Err = String;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -47,7 +48,7 @@ impl FromStr for Dir {
     }
 }
 
-// parse (R|L|U|D) into a DirKind
+/// parse (R|L|U|D) into a DirKind
 impl FromStr for DirKind {
     type Err = String;
 
@@ -61,6 +62,94 @@ impl FromStr for DirKind {
                 "parse::<DirKind>: expected one of R|L|U|D. got {}",
                 s
             ))),
+        }
+    }
+}
+
+// –––––––––– solve the problem
+
+use std::collections::BinaryHeap;
+use std::collections::HashSet;
+
+/// take in a string, output the two locations
+fn find_closest(input: &str) -> Posn {
+    let (one, two) = parse(input);
+    // need to convert the list of deltas to list of positions
+    let posns_list = all_posns(all_corners(one));
+    let posns_set = HashSet::<&Posn>::from_iter(posns_list.iter());
+    let mut collisions = BinaryHeap::new();
+
+    for posn in all_posns(all_corners(two)) {
+        if posns_set.contains(&posn) {
+            collisions.push(posn);
+        }
+    }
+
+    return collisions.pop().unwrap();
+}
+
+/// represents a cartesian coordinate
+#[derive(Eq, PartialEq, Hash, PartialOrd, Clone, Copy)]
+struct Posn {
+    x: i32,
+    y: i32,
+}
+
+/// creates a new posn
+impl Posn {
+    fn new(x: i32, y: i32) -> Posn {
+        Posn { x, y }
+    }
+}
+
+use std::cmp::Ord;
+use std::cmp::Ordering;
+
+/// compare posns by their manhattan distance
+impl Ord for Posn {
+    fn cmp(&self, other: &Self) -> Ordering {
+        (self.x + self.y).cmp(&(other.x + other.y))
+    }
+}
+
+/// convert a list of the corners to a list of the posns. assumes the directions start at 0, 0
+fn all_posns(corners: Vec<Posn>) -> Vec<Posn> {
+    //! this is an innder doc
+    let mut posns = Vec::<Posn>::new();
+    // okay to unwrap, since should always have 0,0
+    let prev = Posn::new(0, 0);
+
+    for corner in corners {
+        add_betweens(&mut posns, &prev, &corner);
+    }
+    return posns;
+}
+
+/// finds the corners of all the dirs. does NOT include 0,0
+fn all_corners(deltas: Vec<Dir>) -> Vec<Posn> {
+    let mut corners: Vec<Posn> = Vec::new();
+    let mut prev = Posn::new(0, 0);
+
+    // for each of the directions, produce the next posn
+    for dir in deltas {
+        let next = match dir.kind {
+            DirKind::Left => Posn::new(prev.x - dir.dist, prev.y),
+            DirKind::Right => Posn::new(prev.x - dir.dist, prev.y),
+            DirKind::Up => Posn::new(prev.x - dir.dist, prev.y),
+            DirKind::Down => Posn::new(prev.x - dir.dist, prev.y),
+        };
+        corners.push(next);
+        prev = next;
+    }
+
+    return corners;
+}
+
+/// adds all the posns between the two to the vector, exclusive of the last. adds them in reverse order
+fn add_betweens(place: &mut Vec<Posn>, from: &Posn, to: &Posn) -> () {
+    for x in to.x..from.x {
+        for y in to.y..from.y {
+            place.push(Posn::new(x, y));
         }
     }
 }
--- src/day03/tests.rs
+++ src/day03/tests.rs
@@ -3,71 +3,71 @@ use crate::day03::*;
 
 #[test]
 fn test_split() {
-  let vec: Vec<&str> = Vec::from_iter("one\ntwo".split("\n"));
+    let vec: Vec<&str> = Vec::from_iter("one\ntwo".split("\n"));
 
-  assert_eq!(vec, vec!["one", "two"]);
+    assert_eq!(vec, vec!["one", "two"]);
 }
 
 #[test]
 fn dirkind_parses() {
-  assert_eq!(DirKind::Left, "L".parse::<DirKind>().unwrap());
-  assert_eq!(DirKind::Right, "R".parse::<DirKind>().unwrap());
-  assert_eq!(DirKind::Up, "U".parse::<DirKind>().unwrap());
-  assert_eq!(DirKind::Down, "D".parse::<DirKind>().unwrap());
+    assert_eq!(DirKind::Left, "L".parse::<DirKind>().unwrap());
+    assert_eq!(DirKind::Right, "R".parse::<DirKind>().unwrap());
+    assert_eq!(DirKind::Up, "U".parse::<DirKind>().unwrap());
+    assert_eq!(DirKind::Down, "D".parse::<DirKind>().unwrap());
 
-  assert_eq!(
-    Err(String::from(
-      "parse::<DirKind>: expected one of R|L|U|D. got x"
-    )),
-    "x".parse::<DirKind>()
-  );
+    assert_eq!(
+        Err(String::from(
+            "parse::<DirKind>: expected one of R|L|U|D. got x"
+        )),
+        "x".parse::<DirKind>()
+    );
 }
 
 #[test]
 fn dir_parses() {
-  assert_eq!(
-    Dir {
-      dist: 10,
-      kind: DirKind::Left,
-    },
-    "L10".parse::<Dir>().unwrap()
-  );
-  assert_eq!(
-    Dir {
-      dist: 1000,
-      kind: DirKind::Up,
-    },
-    "U001000".parse::<Dir>().unwrap()
-  );
+    assert_eq!(
+        Dir {
+            dist: 10,
+            kind: DirKind::Left,
+        },
+        "L10".parse::<Dir>().unwrap()
+    );
+    assert_eq!(
+        Dir {
+            dist: 1000,
+            kind: DirKind::Up,
+        },
+        "U001000".parse::<Dir>().unwrap()
+    );
 }
 
 #[test]
 fn dir_list_parses() {
-  // R10
-  let r10 = Dir {
-    dist: 10,
-    kind: DirKind::Right,
-  };
-  // D99
-  let d99 = Dir {
-    dist: 99,
-    kind: DirKind::Down,
-  };
+    // R10
+    let r10 = Dir {
+        dist: 10,
+        kind: DirKind::Right,
+    };
+    // D99
+    let d99 = Dir {
+        dist: 99,
+        kind: DirKind::Down,
+    };
 
-  assert_eq!(vec![r10, d99], parse_dirs("R10,D099"));
+    assert_eq!(vec![r10, d99], parse_dirs("R10,D099"));
 }
 
 #[test]
 fn two_dir_lists_parse() {
-  // R10
-  let r10 = Dir {
-    dist: 10,
-    kind: DirKind::Right,
-  };
-  // D99
-  let d99 = Dir {
-    dist: 99,
-    kind: DirKind::Down,
-  };
-  assert_eq!((vec![r10], vec![d99]), parse("R10\nD99"));
+    // R10
+    let r10 = Dir {
+        dist: 10,
+        kind: DirKind::Right,
+    };
+    // D99
+    let d99 = Dir {
+        dist: 99,
+        kind: DirKind::Down,
+    };
+    assert_eq!((vec![r10], vec![d99]), parse("R10\nD99"));
 }
