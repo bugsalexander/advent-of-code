e051a3460de8d14b63f256e19e4bb29e1d0986f2
meebs <alex@takayamasan.com>
2020-04-29 12:51:55 -0700

    +test

--- src/day04/tests.rs
+++ src/day04/tests.rs
@@ -5,3 +5,9 @@ use super::*;
 fn oracle_works() {
   assert_eq!(oracle("255", "555"), 30);
 }
+
+#[test]
+fn part_two_works() {
+  assert_eq!(oracle("123444", "123444"), 0);
+  assert_eq!(oracle("111122", "111122"), 1);
+}
