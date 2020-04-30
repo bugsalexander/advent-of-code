8ae8307c6a499a37b2d6c64c848a98f9ff95bfde
meebs <alex@takayamasan.com>
2020-04-30 00:16:20 -0700

    fix a test

--- src/day04/tests.rs
+++ src/day04/tests.rs
@@ -3,11 +3,12 @@ use super::*;
 
 #[test]
 fn oracle_works() {
-  assert_eq!(oracle("255", "555"), 30);
+    // this test was for pt 1
+    // assert_eq!(oracle("255", "555"), 30);
 }
 
 #[test]
 fn part_two_works() {
-  assert_eq!(oracle("123444", "123444"), 0);
-  assert_eq!(oracle("111122", "111122"), 1);
+    assert_eq!(oracle("123444", "123444"), 0);
+    assert_eq!(oracle("111122", "111122"), 1);
 }
