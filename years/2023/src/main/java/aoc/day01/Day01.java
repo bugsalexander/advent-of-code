dbe4d15a561009f2ce1f5501ab2cdc4fa5c96b63
Alex Takayama <22948432+bugsalexander@users.noreply.github.com>
2023-12-18 09:58:19 -0800

    day 1 part 1

--- years/2023/src/main/java/aoc/day01/Day01.java
+++ years/2023/src/main/java/aoc/day01/Day01.java
@@ -1,14 +1,21 @@
 package aoc.day01;
 
-import aoc.Day;
-
 import java.util.List;
+import aoc.Day;
 
 public class Day01 implements Day {
 
     @Override
     public String part1(List<String> input) {
-        return input.isEmpty() ? "" : input.get(0);
+        // first digit and last digit to make a 2-digit number
+        // each input is a line
+        int total = input.stream().mapToInt(line -> {
+            List<Integer> digits = line.codePoints().filter(Character::isDigit).boxed().toList();
+            int first = Character.getNumericValue(digits.get(0));
+            int last = Character.getNumericValue(digits.get(digits.size() - 1));
+            return (first * 10) + last;
+        }).sum();
+        return String.valueOf(total);
     }
 
     @Override
