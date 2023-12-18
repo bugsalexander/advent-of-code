416acdfdf92563c97a02a87075a5b6ebafb60f69
Alex Takayama <22948432+bugsalexander@users.noreply.github.com>
2023-12-18 09:58:19 -0800

    day 3 template

---
+++ years/2023/src/main/java/aoc/day03/Day03.java
@@ -0,0 +1,20 @@
+/* **********************************************************************
+ * Copyright 2023 VMware, Inc.  All rights reserved. VMware Confidential
+ * *********************************************************************/
+
+package aoc.day03;
+
+import aoc.Day;
+import java.util.List;
+
+public class Day03 implements Day {
+    @Override
+    public String part1(List<String> input) {
+        return null;
+    }
+
+    @Override
+    public String part2(List<String> input) {
+        return null;
+    }
+}
---
+++ years/2023/src/test/java/aoc/day03/Day03Test.java
@@ -0,0 +1,15 @@
+/* **********************************************************************
+ * Copyright 2023 VMware, Inc.  All rights reserved. VMware Confidential
+ * *********************************************************************/
+
+package aoc.day03;
+
+import org.testng.annotations.Test;
+
+public class Day03Test {
+
+    @Test
+    public void testPart1() {
+
+    }
+}
