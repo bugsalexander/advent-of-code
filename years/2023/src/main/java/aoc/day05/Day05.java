ca7b59a73d20a7dfd2e0de906dc5a518dddd9a5a
Alex Takayama <22948432+bugsalexander@users.noreply.github.com>
2023-12-19 00:33:11 -0800

    day5 part2 brute force SUPER brute force. literally waited 20 mins for this to run jk it took 52 mins. the rate slowed a bunch at the end.

--- years/2023/src/main/java/aoc/day05/Day05.java
+++ years/2023/src/main/java/aoc/day05/Day05.java
@@ -7,15 +7,24 @@ package aoc.day05;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import aoc.Day;
 
 public class Day05 implements Day {
+
+    Map<SeedRequirementType, Mapping> maps;
 
     @Override
     public String part1(List<String> input) {
@@ -24,7 +33,9 @@ public class Day05 implements Day {
 
         // should only be one line. start after "seeds: "
         List<BigInteger> seeds = parseNumbers(groups.remove(0).get(0).substring(7));
-        return computeMinLocation(seeds.iterator(), groups, BigInteger.valueOf(seeds.size())).toString();
+        maps = groups.stream().map(Mapping::fromString)
+                .collect(Collectors.toMap(Mapping::getSourceType, Function.identity()));
+        return computeMinLocation(seeds.iterator()).toString();
     }
 
     @Override
@@ -33,38 +44,72 @@ public class Day05 implements Day {
 
         // should only be one line. start after "seeds: "
         List<BigInteger> seedRanges = parseNumbers(groups.remove(0).get(0).substring(7));
-        BigInteger total = BigInteger.ZERO;
-        for (int i = 1; i < seedRanges.size(); i += 2) {
-            total = total.add(seedRanges.get(i));
+        List<Callable<BigInteger>> tasks = new ArrayList<>();
+        for (int i = 0; i < seedRanges.size(); i += 2) {
+            Iterator<BigInteger> iterator = new SeedRangeIterator(seedRanges.subList(i, i + 2));
+            BigInteger total = seedRanges.get(i + 1);
+            tasks.add(() -> computeMinLocationAndLog(iterator, total));
         }
-        // this actually corresponds to a list of pairs, where the first is the start, and the second is the range
-        Iterator<BigInteger> seedRangeIterator = new SeedRangeIterator(seedRanges);
 
-        return computeMinLocation(seedRangeIterator, groups, total).toString();
-    }
-
-    private BigInteger computeMinLocation(Iterator<BigInteger> seeds, List<List<String>> groups, BigInteger total) {
-        Map<SeedRequirementType, Mapping> maps = groups.stream().map(Mapping::fromString)
+        maps = groups.stream().map(Mapping::fromString)
                 .collect(Collectors.toMap(Mapping::getSourceType, Function.identity()));
 
-        BigInteger done = BigInteger.ZERO;
-        BigInteger runningMin = computeMinLocation(seeds.next(), maps);
-        while (seeds.hasNext()) {
-            BigInteger current = computeMinLocation(seeds.next(), maps);
-            runningMin = runningMin.min(current);
-            done = done.add(BigInteger.ONE);
-            System.out.printf("%s / %s\n", done, total.toString());
+        ExecutorService executor = Executors.newWorkStealingPool();
+        CompletionService<BigInteger> completionService = new ExecutorCompletionService<>(executor);
+        tasks.forEach(completionService::submit);
+
+        int done = 0;
+        BigInteger min;
+        try {
+            min = completionService.take().get();
+            done += 1;
+            while (done < tasks.size()) {
+                min = min.min(completionService.take().get());
+                done += 1;
+                System.out.printf("tasks completed: %s / %s\n", done, tasks.size());
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
         }
 
-        // 134830 / second (no caching)
-        // 108817 / second (caching)
+        return min.toString();
+    }
+
+    private BigInteger computeMinLocationAndLog(Iterator<BigInteger> seeds, BigInteger total) {
+        BigInteger[] done = { BigInteger.ZERO };
+
+        // periodically report progress
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.printf("%s / %s\n", done[0], total);
+            }
+        }, 10 * 1000L, 10 * 1000L);
+
+        BigInteger min = computeMinLocationForSeed(seeds.next());
+        done[0] = done[0].add(BigInteger.ONE);
+        while (seeds.hasNext()) {
+            min = min.min(computeMinLocationForSeed(seeds.next()));
+            done[0] = done[0].add(BigInteger.ONE);
+        }
+
+        timer.cancel();
+        return min;
+    }
+    private BigInteger computeMinLocation(Iterator<BigInteger> seeds) {
+        BigInteger runningMin = computeMinLocationForSeed(seeds.next());
+        while (seeds.hasNext()) {
+            BigInteger current = computeMinLocationForSeed(seeds.next());
+            runningMin = runningMin.min(current);
+        }
 
         return runningMin;
     }
 
-    private BigInteger computeMinLocation(BigInteger seed, Map<SeedRequirementType, Mapping> maps) {
+    private BigInteger computeMinLocationForSeed(BigInteger seed) {
         TypeValuePair pair = new TypeValuePair(SeedRequirementType.Seed, seed);
-        return mapToLocation(pair, maps);
+        return mapToLocation(pair);
     }
 
     private static List<List<String>> getGroups(List<String> input) {
@@ -87,7 +132,7 @@ public class Day05 implements Day {
         return groups;
     }
 
-    private BigInteger mapToLocation(TypeValuePair startPair, Map<SeedRequirementType, Mapping> maps) {
+    private BigInteger mapToLocation(TypeValuePair startPair) {
         TypeValuePair currentPair = startPair;
         while (currentPair.getType() != SeedRequirementType.Location) {
             Mapping mapping = maps.get(currentPair.getType());
