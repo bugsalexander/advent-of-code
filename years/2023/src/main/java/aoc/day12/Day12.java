403f9e597ec35578ec9555177c8ceb502a2bed54
Alex Takayama <22948432+bugsalexander@users.noreply.github.com>
2023-12-26 00:34:51 -0800

    day12 part1 but it doesn't work :(

--- years/2023/src/main/java/aoc/day12/Day12.java
+++ years/2023/src/main/java/aoc/day12/Day12.java
@@ -15,7 +15,7 @@ import org.apache.commons.lang3.tuple.Pair;
 public class Day12 implements Day {
     @Override
     public String part1(List<String> input) {
-        List<Pair<SpringCondition[], List<Integer>>> lines = parse(input);
+        List<Pair<List<SpringCondition>, List<Integer>>> lines = parse(input);
         BigInteger total = countPossibleArrangements(lines);
         return total.toString();
     }
@@ -25,58 +25,107 @@ public class Day12 implements Day {
         return null;
     }
 
-    private List<Pair<SpringCondition[], List<Integer>>> parse(List<String> input) {
-        return input.stream()
-                .map(this::parseLine)
-                .collect(Collectors.toList());
+    private List<Pair<List<SpringCondition>, List<Integer>>> parse(List<String> input) {
+        return input.stream().map(this::parseLine).collect(Collectors.toList());
     }
 
-    private Pair<SpringCondition[], List<Integer>> parseLine(String line) {
+    private Pair<List<SpringCondition>, List<Integer>> parseLine(String line) {
         String[] parts = line.split(" ");
-        SpringCondition[] springs = parts[0].chars()
-                .mapToObj(SpringCondition::fromChar)
-                .toArray(SpringCondition[]::new);
-        List<Integer> brokenGroups = Arrays.stream(parts[1].split(","))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<SpringCondition> springs = parts[0].chars().mapToObj(SpringCondition::fromChar).collect(Collectors.toList());
+        List<Integer> brokenGroups = parts.length < 2 ? List.of() :
+                Arrays.stream(parts[1].split(",")).map(Integer::parseInt).collect(Collectors.toList());
         return Pair.of(springs, brokenGroups);
     }
 
-    private BigInteger countPossibleArrangements(List<Pair<SpringCondition[], List<Integer>>> lines) {
-        return lines.stream()
-                .map(this::countPossibleArrangements)
-                .reduce(BigInteger.ZERO, BigInteger::add);
+    private BigInteger countPossibleArrangements(List<Pair<List<SpringCondition>, List<Integer>>> lines) {
+        return lines.stream().map(this::countPossibleArrangements).reduce(BigInteger.ZERO, BigInteger::add);
     }
 
-    private BigInteger countPossibleArrangements(Pair<SpringCondition[], List<Integer>> pair) {
-        SpringCondition[] springs = pair.getLeft();
+    private BigInteger countPossibleArrangements(Pair<List<SpringCondition>, List<Integer>> pair) {
+        List<SpringCondition> springs = pair.getLeft();
         List<Integer> brokenGroups = pair.getRight();
-        // dp[type][i] = total possible arrangements, if type = type at index = i
-        int[][] dp = new int[SpringCondition.values().length][pair.getLeft().length];
 
-        // initialize dp
-        SpringCondition current = springs[0];
-        if (current == SpringCondition.Unknown) {
-            for (SpringCondition type : SpringCondition.values()) {
-                // one potential solution so far
-                dp[type.ordinal()][0] = 1;
-            }
-        } else {
-            for (SpringCondition type : SpringCondition.values()) {
-                // zero solutions if type does not match current
-                dp[type.ordinal()][0] = current == type ? 1 : 0;
+        int total = pluginBrokenGroups(springs, 0, brokenGroups, 0);
+        System.out.println(total);
+        return BigInteger.valueOf(total);
+    }
+
+    private int pluginBrokenGroups(List<SpringCondition> springs, int springIndex, List<Integer> brokenGroups, int brokenGroupIndex) {
+        // base case, we have the last spring
+        if (springIndex == springs.size() - 1) {
+            switch(springs.get(springIndex)) {
+            case Operational:
+                if (brokenGroupIndex >= brokenGroups.size()) {
+                    // if no broken groups left, yay!
+                    return 1;
+                } else {
+                    // we must use another broken group, but there are no more damaged gears
+                    return 0;
+                }
+            case Damaged:
+                // if no more broken groups left, or there is but it requires more than 1 gear, no solution
+                if (brokenGroupIndex >= brokenGroups.size() || brokenGroups.get(brokenGroupIndex) != 1) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            case Unknown:
+                // one solution if there is no group left, or a single group with size 1 left
+                if (brokenGroupIndex >= brokenGroups.size()
+                        || (brokenGroupIndex == brokenGroups.size() - 1 && brokenGroups.get(brokenGroupIndex) == 1)) {
+                    return 1;
+                } else {
+                    return 0;
+                }
             }
         }
 
-        // iterate through setting dp as we go
-        for (int i = 1; i < springs.length; i += 1) {
-            current = springs[i];
-            for (SpringCondition type : SpringCondition.values()) {
-                if (current != type) {
-                    // zero solutions where the previous char is
-                    dp[type.ordinal()][i] = 0;
-                }
+        // we either use a broken group here, or we don't
+        switch (springs.get(springIndex)) {
+        case Operational:
+            // we don't
+            return pluginBrokenGroups(springs, springIndex + 1, brokenGroups, brokenGroupIndex);
+        case Damaged:
+            return pluginBrokenGroup(springs, springIndex, brokenGroups, brokenGroupIndex);
+        case Unknown:
+            // we may or may not use a broken group
+            int usingBrokenGroup = pluginBrokenGroup(springs, springIndex, brokenGroups, brokenGroupIndex);
+            int notUsingBrokenGroup = pluginBrokenGroups(springs, springIndex + 1, brokenGroups, brokenGroupIndex);
+            return usingBrokenGroup + notUsingBrokenGroup;
+        }
+        throw new IllegalStateException("to satisfy compiler");
+    }
+
+    // assume we must use a broken group now
+    public int pluginBrokenGroup(List<SpringCondition> springs, int springIndex, List<Integer> brokenGroups, int brokenGroupIndex) {
+        // no more broken groups, or the size of the broken group is bigger than remaining spring slots
+        if (brokenGroupIndex >= brokenGroups.size() || brokenGroups.get(brokenGroupIndex) + springIndex > springs.size()) {
+            return 0;
+        }
+        Integer brokenGroup = brokenGroups.get(brokenGroupIndex);
+        int nextIndex = springIndex + brokenGroup;
+        if (nextIndex < springs.size() && springs.get(nextIndex) == SpringCondition.Damaged) {
+            // the spring following the broken group must be nonexistent or operational
+            // check this first since it is O(1) < O(groupSize)
+            return 0;
+        }
+        for (SpringCondition type : springs.subList(springIndex + 1, nextIndex)) {
+            if (type == SpringCondition.Operational) {
+                // if we cannot use the broken group, then we are done. zero possibilities
+                return 0;
             }
+        }
+
+        if (nextIndex + 1 >= springs.size()) {
+            if (brokenGroupIndex + 1 >= brokenGroups.size()) {
+                // if we are done, and there are no more groups, we are done
+                return 1;
+            } else {
+                // if we are done and there are remaining broken groups, no solution
+                return 0;
+            }
+        } else {
+            return pluginBrokenGroups(springs, nextIndex + 1, brokenGroups, brokenGroupIndex + 1);
         }
     }
 }
--- years/2023/src/test/java/aoc/day12/Day12Test.java
+++ years/2023/src/test/java/aoc/day12/Day12Test.java
@@ -4,21 +4,96 @@
 
 package aoc.day12;
 
-import aoc.Day;
-import aoc.DayTest;
-
 import org.testng.annotations.Test;
 
+import aoc.DayTest;
+
 public class Day12Test extends DayTest {
-    private static final String input = "???.### 1,1,3\n" + ".??..??...?##. 1,1,3\n" + "?#?#?#?#?#?#?#? 1,3,1,6\n" + "????.#...#... 4,1,1\n" + "????.######..#####. 1,6,"
-            + "5\n"
-            + "?###???????? 3,2,1";
+    private static final String input =
+            "???.### 1,1,3\n" + ".??..??...?##. 1,1,3\n" + "?#?#?#?#?#?#?#? 1,3,1,6\n" + "????.#...#... 4,1,1\n" + "????.######..#####. 1,6," + "5\n"
+                    + "?###???????? 3,2,1";
+
     protected Day12Test() {
         super(new Day12(), input);
     }
 
     @Test
+    public void part1Base() {
+        testPart1(".", "1");
+        testPart1("# 1", "1");
+        testPart1("? 1", "1");
+        testPart1("?", "1");
+
+        testPart1(". 1", "0");
+        testPart1("#", "0");
+        testPart1("? 2", "0");
+    }
+
+    @Test
+    public void part1Step() {
+        testPart1(".?", "1");
+        testPart1(".? 1", "1");
+        testPart1(".? 2", "0");
+        testPart1("..", "1");
+        testPart1(".. 1", "0");
+        testPart1(".#", "0");
+        testPart1(".# 1", "1");
+        testPart1(".# 2", "0");
+
+        testPart1("#?", "0");
+        testPart1("#? 1", "1");
+        testPart1("#? 2", "1");
+        testPart1("#? 3", "0");
+        testPart1("#.", "0");
+        testPart1("#. 1", "1");
+        testPart1("#. 2", "0");
+        testPart1("##", "0");
+        testPart1("## 1", "0");
+        testPart1("## 2", "1");
+        testPart1("## 3", "0");
+
+        testPart1("??", "1");
+        testPart1("?? 1", "2");
+        testPart1("?? 2", "1");
+        testPart1("?? 3", "0");
+        testPart1("?? 1,1", "0");
+        testPart1("?? 2,1", "0");
+        testPart1("?? 1,2", "0");
+        testPart1("?.", "1");
+        testPart1("?. 1", "1");
+        testPart1("?. 2", "0");
+        testPart1("?#", "0");
+        testPart1("?# 1", "1");
+        testPart1("?# 2", "1");
+        testPart1("?# 3", "0");
+    }
+
+    @Test
+    public void part1Step1() {
+        testPart1("??? 1,1", "1");
+    }
+
+    @Test
     public void testPart1() {
+        // each line by line of sample input
+        testPart1("???.### 1,1,3", "1");
+        testPart1(".??..??...?##. 1,1,3", "4");
+        testPart1("?#?#?#?#?#?#?#? 1,3,1,6", "1");
+        testPart1("????.#...#... 4,1,1", "1");
+        testPart1("????.######..#####. 1,6,5", "4");
+        testPart1("?###???????? 3,2,1", "10");
         testPart1("21");
+    }
+
+    @Test
+    public void testPart1NoUnknown() {
+        String input = "#.#.### 1,1,3\n" + ".#...#....###. 1,1,3\n" + ".#.###.#.###### 1,3,1,6\n" + "####.#...#... 4,1,1\n" + "#....######..#####. 1,6,5\n"
+                + ".###.##....# 3,2,1";
+        testPart1(input, "6");
+    }
+
+    @Test
+    public void fromInput() {
+        testPart1("?#??#??????????.??.? 7,2,3,1", "12");
     }
 }
